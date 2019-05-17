271096b1e3413bafe12b3cd7fcf505c870c5ea68
Juan Pablo Vallejo <juanpv@lagash.com>
2019-05-17 01:24:09 -0300

    Add create test

--- Tests/ContactTests.cs
+++ Tests/ContactTests.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
 using System;
+using System.Net;
 using WebApi.Controllers;
+using WebApi.Models;
 
 namespace Tests
 {
@@ -27,6 +29,27 @@ namespace Tests
             var id = Guid.NewGuid().ToString();
             var result = controller.Get(id);
             Assert.That(result.Value == "Not Found");
+        }
+
+        [Test]
+        public void TestCreateContactWithCorrectValuesCreates()
+        {
+            var controller = new ContactsController();
+            var model = new ContactViewModel()
+            {
+                Name = "John Peres",
+                Birthday = "28/02/1998",
+                Email = "johnp@gmail.com",
+                Company = "Fake Company",
+                PersonalPhone = "1122009988",
+                WorkPhone = "1122119988",
+                Address = "Fake St. 123",
+                City = "Buenos Aires",
+                State = "Buenos Aires"
+            };
+            Guid guid;
+            var result = controller.Create(model);
+            Assert.That(result.StatusCode == (int)HttpStatusCode.Created && Guid.TryParse(result.Value.ToString(),out guid) );
         }
     }
 }
